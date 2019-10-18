bdd4c0366bd12b73cacaa0931c612616dba2ae87
paydaylight <maxat235@gmail.com>
2019-10-18 19:59:58 +0600

    added semantic

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import SideBarItems from './SideBarItems'
 
 class SideBar extends React.Component {
     render() {
         return(
-            <div>
-
+            <div className="ui fixed vertical inverted menu" styleName="width: 150px; height: 100%; overflow: auto; cursor: pointer;">
+                <SideBarItems />
             </div>
         )
     }
---
+++ src/components/SideBarItems.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class SideBarItems extends React.Component {
+    render() {
+        return(
+            <div>
+                <a class="item" href="#">
+                    <img src="#"></img>
+                </a>
+                <a class="item" href="">
+                    Профиль
+                </a>
+                <a class="item" href="#">
+                    Заявки наставников
+                </a>
+                <a class="item" href="#">
+                    Наставники
+                </a>
+                <a class="item" href="#">
+                    Чат
+                </a>
+                <a class="item" href="#">
+                    Отчёты
+                </a>
+                <a class="item" href="#">
+                    Встречи
+                </a>
+                <a class="item" href="#">
+                    Детские дома
+                </a>
+                <a class="item" href="#">
+                    Дети
+                </a>
+                <a class="item" href="#">
+                    Кураторы
+                </a>
+            </div>
+        )
+    }
+}
+
+export default SideBarItems
