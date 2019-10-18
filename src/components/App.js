1389516a13a95f713e124c607d83bdf92558a0df
paydaylight <maxat235@gmail.com>
2019-10-18 19:23:04 +0600

    added sidebar component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SideBar from './SideBar';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SideBar extends React.Component {
+    render() {
+        <div>
+
+        </div>
+    }
+}
+
+export default SideBar
